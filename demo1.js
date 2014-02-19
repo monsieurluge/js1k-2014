b5264ef28cafa52d408b418fdc0e77fcf7aa31cc
MonsieurLuge <monsieurluge@gmail.com>
2014-02-19 13:19:54 +0000

    Demo1 : add some improvements

---
+++ demo1.js
@@ -0,0 +1,149 @@
+/**
+ * JS1K 2014
+ * http://js1k.com/2014-dragons/
+ * 
+ * Demo author: monsieurluge
+ * Demo name : xxxxx
+**/
+
+//init
+level = step = 0;
+arenaSize = 500;
+
+//add the player
+//type, x, y, dx, dy, zoom, speed, timebeforefire, tiles, size
+objects = [['p', 9, 9, 0, 0, 4, 3, 0, '04403334411001000440433301140010', 4]];
+player = objects[0];
+
+//keys
+//left=37 up=38 right=39 down=40
+b.onkeydown=function(event) {
+	keyCode = event.keyCode;
+    keyCode == 37 || keyCode == 39 ? player[3] = keyCode-38 : 0;
+    keyCode == 38 || keyCode == 40 ? player[4] = keyCode-39 : 0;
+};
+
+b.onkeyup=function(event) {
+    keyCode = event.keyCode;
+    keyCode == 37 || keyCode == 39 ? player[3] = 0 : 0;
+    keyCode == 38 || keyCode == 40 ? player[4] = 0 : 0;
+};
+
+//new game
+function newGame() {
+    level++;
+
+    //add the ennemies
+    //type, x, y, dx, dy, zoom, speed, timebeforefire, tiles, size
+    for (object = 0; object < level; object++)
+        objects.push(['e', Math.random()*arenaSize, Math.random()*arenaSize, 0, 0, 5+Math.random()*level, 1.1, Math.random()*80, '10210111111010101021011311101010', 4]);
+}
+
+//fires a bullet
+function newBullet(origintype, originx, originy, targetx, targety, speed, zoom) {
+    dx = targetx-originx;
+    dy = targety-originy;
+    
+    if (Math.abs(dx) < Math.abs(dy)) {
+        dx = dx/Math.abs(dy);
+        dy = dy/Math.abs(dy);
+    } else {
+        dy = dy/Math.abs(dx);
+        dx = dx/Math.abs(dx);
+    }
+    
+    //create the bullet
+    //type, x, y, dx, dy, zoom, speed, N#A, tiles, size, origin
+    objects.push(['b', originx, originy, dx, dy, zoom, speed, 0, '43', 1, origintype]);
+}
+
+//collision
+function isCollision(obj, target) {
+    objx = obj[1],
+    objy = obj[2],
+    objz = obj[5],
+    targetx = target[1],
+    targety = target[2],
+    targetz = target[5];
+
+    return !(targetx >= objx+objz || targetx+targetz <= objx || targety >= objy+objz || targety+targetz <= objy);
+}
+
+//destroy an object
+function destroy(obj) {
+    objects.splice(objects.indexOf(obj), 1);
+}
+
+//player fire a bullet
+a.addEventListener('click', function(event) {
+    if (player[7] < 0) {
+        newBullet('p', player[1], player[2], event.x, event.y, 6, 4);
+        player[7] = 20;
+    }
+}, 0);
+
+//cycle
+interval = setInterval(function () {
+    //clear the scene
+    a.width += 0;
+
+    //show the score
+    c.fillText('lvl' + level, 5, 15);
+
+    //manage objects
+    objects.forEach(function(obj) {
+        //reduce the 'next time before firing again'
+        obj[7]--;
+        
+        //ennemy
+        if (obj[0] == 'e') {
+            //move to the player
+            obj[3] = obj[1] < player[1] ? 1 : -1;
+            obj[4] = obj[2] < player[2] ? 1 : -1;
+
+            if (obj[7] < 0) {
+                //fire a bullet
+                newBullet('e', obj[1], obj[2], player[1], player[2], obj[6]*4, obj[5]);
+                obj[7] = 30;
+            }
+        }
+
+        //bullet
+        if (obj[0] == 'b') {
+            //check for bullet collision
+            objects.forEach(function(target) {
+                //collision between a player bullet and an ennemy ?
+                if (obj[10] == 'p' && target[0] == 'e' && isCollision(obj, target)) {
+                    destroy(target);
+                    destroy(obj);
+                }
+                
+                //collision between an emmeny bullet and the player ?
+                obj[10] == 'e' && target[0] == 'p' && isCollision(obj, target) ? clearInterval(interval) : 0;
+            });
+
+            //destroy the bullet if out of the arena
+            obj[1] < 0 || obj[1] > arenaSize || obj[2] < 0 || obj[2] > arenaSize ? destroy(obj) : 0;
+        }
+
+        //move the object
+        obj[1] += obj[3]*obj[6];
+        obj[2] += obj[4]*obj[6];
+
+        //draw the object
+        zoom = obj[5];
+        size = obj[9];
+
+        for(i = 0; i < size*size; i++) {
+            tile = obj[8].charAt(i+size*size*(step%20 == 0));
+            c.fillStyle = '#' + ['FFF','294','000','E21','DAA'][tile]; //colors : white,green,black,red,flesh
+            c.fillRect(obj[1]+zoom*(i%size), obj[2]+zoom*(Math.floor(i/size)), zoom, zoom);
+        }
+
+        //next frame
+        step++;
+        
+        //new game ?
+        objects.length < 2 ? newGame() : 0;
+    });
+},33);
---
+++ demo1min.js
@@ -0,0 +1,160 @@
+/**
+ * JS1K 2014
+ * http://js1k.com/2014-dragons/
+ * 
+ * Demo author: monsieurluge
+ * Demo name : xxxxx
+**/
+
+//misc
+M=Math,
+R=M.random,
+A=M.abs,
+W=500; //arena width & height
+
+//init
+//L:level
+//S:step
+L = S = 0;
+
+//add the player
+//type, x, y, dx, dy, zoom, speed, timebeforefire, tiles, size
+//O:objects
+//P:player
+O = [['p', 9, 9, 0, 0, 4, 3, 0, '04403334411001000440433301140010', 4]];
+P = O[0];
+
+//keys
+//left=37 up=38 right=39 down=40
+b.onkeydown=function(e) {
+	k = e.keyCode;
+    k == 37 || k == 39 ? P[3] = k-38 : 0;
+    k == 38 || k == 40 ? P[4] = k-39 : 0;
+};
+
+b.onkeyup=function(e) {
+    k = e.keyCode;
+    k == 37 || k == 39 ? P[3] = 0 : 0;
+    k == 38 || k == 40 ? P[4] = 0 : 0;
+};
+
+//new game
+function G() {
+    L++;
+    
+    //add the ennemies
+    //type, x, y, dx, dy, zoom, speed, timebeforefire, tiles, size
+    for (o = 0; o < L; o++)
+        O.push(['e', R()*W, R()*W, 0, 0, 5+R()*L, 1.1, R()*80, '10210111111010101021011311101010', 4]);
+}
+
+//fires a bullet
+//origintype, originx, originy, targetx, targety, speed, zoom
+function B(o, x, y, v, w, s, z) {
+    i = v-x;
+    j = w-y;
+    
+    if (A(i) < A(j)) {
+        i = i/A(j);
+        j = j/A(j);
+    } else {
+        j = j/A(i);
+        i = i/A(i);
+    }
+    
+    //create the bullet
+    //type, x, y, dx, dy, zoom, speed, N#A, tiles, size, origin
+    O.push(['b', x, y, i, j, z, s, 0, '43', 1, o]);
+}
+
+//collision
+function C(o, t) {
+    x = o[1],
+    y = o[2],
+    z = o[5],
+    v = t[1],
+    w = t[2],
+    u = t[5];
+
+    return !(v >= x+z || v+u <= x || w >= y+z || w+u <= y);
+}
+
+//destroy an object
+function D(o) {
+    O.splice(O.indexOf(o), 1);
+}
+
+//player fire a bullet
+a.addEventListener('click', function(e) {
+    if (P[7] < 0) {
+        B('p', P[1], P[2], e.x, e.y, 6, 4);
+        P[7] = 20;
+    }
+}, 0);
+
+//cycle
+I = setInterval(function () {
+    //clear the scene
+    a.width += 0;
+
+    //show the score
+    c.fillText('lvl' + L, 5, 15);
+
+    //manage objects
+    O.forEach(function(o) {
+        //reduce the 'next time before firing again'
+        o[7]--;
+
+        //ennemy
+        if (o[0] == 'e') {
+            //move to the player
+            o[3] = o[1] < P[1] ? 1 : -1;
+            o[4] = o[2] < P[2] ? 1 : -1;
+
+            if (o[7] < 0) {
+                //fire a bullet
+                B('e', o[1], o[2], P[1], P[2], o[6]*4, o[5]);
+                o[7] = 30;
+            }
+        }
+
+        //bullet
+        if (o[0] == 'b') {
+            //check for bullet collision
+            O.forEach(function(t) {
+                //collision between a player bullet and an ennemy ?
+                if (o[10] == 'p' && t[0] == 'e' && C(o, t)) {
+                    D(t);
+                    D(o);
+                    S++;
+                }
+                
+                //collision between an emmeny bullet and the player ?
+                o[10] == 'e' && t[0] == 'p' && C(o, t) ? clearInterval(I) : 0;
+            });
+
+            //destroy the bullet if out of the arena
+            o[1] < 0 || o[1] > W || o[2] < 0 || o[2] > W ? D(o) : 0;
+        }
+
+        //move the object
+        o[1] += o[3]*o[6];
+        o[2] += o[4]*o[6];
+
+        //draw the object
+        z = o[5];
+        s = o[9];
+
+        for(i = 0; i < s*s; i++) {
+            t = o[8].charAt(i+s*s*(S%20 == 0));
+            c.fillStyle = '#' + ['FFF','294','000','E21','DAA'][t]; //colors : white,green,black,red,flesh
+            c.fillRect(o[1]+z*(i%s), o[2]+z*(M.floor(i/s)), z, z);
+        }
+
+        //next frame
+        S++;
+        
+        //new game ?
+        O.length < 2 ? G() : 0;
+    });
+},33);
